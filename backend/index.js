a5352d520c0c567911c7dcc941c03cc93771acdb
Karrar <51439971+KarrarAaljaber@users.noreply.github.com>
2022-04-04 02:19:24 +0200

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
-const app = require("express")();
+
+const express = require('express');
+const app = express();
+const path = require("path");
+
 const server = require("http").createServer(app);
 const cors = require("cors");
 
@@ -11,6 +15,14 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "../react-frontend/build")));
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+/*
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
@@ -34,3 +46,4 @@ io.on("connection", (socket) => {
 });
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+*/
