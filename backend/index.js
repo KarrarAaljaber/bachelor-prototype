5a873b84751dc9f99da6f39984f2671ab2ba53fe
KarrarAaljaber <karrar_adel99@yahoo.com>
2022-04-04 17:48:29 -0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,9 @@
 
-const express = require('express');
-const app = express();
+
+const express = require("express");
 const path = require("path");
 
-const server = require("http").createServer(app);
-const cors = require("cors");
-
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "*",
-		methods: [ "GET", "POST" ]
-	}
-});
-
-app.use(cors());
+const app = express();
 
 app.use(express.static(path.join(__dirname, "../react-frontend/build")));
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
@@ -22,28 +12,3 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-/*
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-	res.send('Running');
-});
-
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
-
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
-});
-
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-*/
--- react-frontend/src/components/Appbar.js
+++ react-frontend/src/components/Appbar.js
@@ -7,7 +7,7 @@ export default function Appbar() {
   return (
     <>
     <AppBar position="static" color="primary">
-            <Typography variant="h4" align="center"  > Telelogepdi Nettportal Prototype </Typography>
+            <Typography variant="h4" align="center"  > Telelogopedi Nettportal Prototype </Typography>
         </AppBar>
     </>
   );
