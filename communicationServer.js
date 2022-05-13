8d0739b77388d69fb38a919c51b4ec9cf7d781aa
Karrar <51439971+KarrarAaljaber@users.noreply.github.com>
2022-05-13 02:52:49 +0200

    commit

--- communicationServer.js
+++ communicationServer.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://nettportal.netlify.app/",
+		origin: "*",
 		methods: [ "GET", "POST" ]
 	}
 });
@@ -13,6 +13,31 @@ const io = require("socket.io")(server, {
 
 
 app.use(cors());
+
+
+const corsMiddleware = (req, res, next) => {
+	res = applyCorsHeaders(res);
+	if (req.method === 'OPTIONS') {
+	  res.status(200).end()
+	  return
+	}
+	next()
+  }
+  
+  const applyCorsHeaders = res => {
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// or res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+	res.setHeader(
+	  'Access-Control-Allow-Headers',
+	  'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	)
+	return res;
+  }
+
+  app.use(corsMiddleware);
+
 
 const PORT = process.env.PORT || 5000;
 
